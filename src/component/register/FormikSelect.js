eced954e1d0dd863e79778d88a5b1d935fbc8724
donia-elsayed <78657116+donia-elsayed@users.noreply.github.com>
2021-10-24 20:04:03 +0200

    Update Register Form

--- src/component/register/FormikSelect.js
+++ src/component/register/FormikSelect.js
@@ -2,15 +2,17 @@ import {Field} from 'formik';
 import FormikErrorMessage from "./FormikErrorMessage";
 function FormikSelect({label,name}) {
     return (
-        <div className="mb-2 text-start ps-3 d-flex">
-            <label htmlFor={name} className="form-label me-5 fs-5">{label}</label>
-            <Field as="select" name={name} className="form-select w-25">
-                <option value=""></option>
-                <option value="Male" >Male</option>
-                <option value="Female">Female</option>
-            </Field>
-            <FormikErrorMessage name={name} />
-        </div>
+       <>
+            <div className="my-3 text-start ps-3 d-flex">
+                <label htmlFor={name} className="form-label me-5 fs-5">{label}</label>
+                <Field as="select" name={name} className="form-select w-25">
+                    <option value=""></option>
+                    <option value="Male" >Male</option>
+                    <option value="Female">Female</option>
+                </Field>  
+            </div>
+            <FormikErrorMessage name={name}/>
+        </>
     )
 }
 
